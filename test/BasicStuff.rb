5ef69c4f801c573aadcf7692f0577e967ebf32eb
Christian Breitkreutz <cbreitkreutz@epages.com>
2018-01-10 21:55:19 +0100

    condition

--- test/BasicStuff.rb
+++ test/BasicStuff.rb
@@ -3,13 +3,30 @@ require 'test/unit'
 class BasicStuff < Test::Unit::TestCase
   def test_stringConcat
     ruby = 'Ruby'
-    assert_equal('Hello '+ ruby,'Hello Ruby', '+ concatenation went wrong')
-    assert_equal("Hello #{ruby}",'Hello Ruby', 'double quote #{} concatenation went wrong')
-    assert_equal('Hello ' << ruby,'Hello Ruby', '<< concatenation went wrong')
-    assert_equal("Hello #{ruby}" + ', how are' << ' you?','Hello Ruby, how are you?', 'concatenation mix went wrong')
+    assert_equal('Hello Ruby', 'Hello '+ ruby, '+ concatenation went wrong')
+    assert_equal('Hello Ruby', "Hello #{ruby}", 'double quote #{} concatenation went wrong')
+    assert_equal('Hello Ruby', 'Hello ' << ruby, '<< concatenation went wrong')
+    assert_equal('Hello Ruby, how are you?',
+      "Hello #{ruby}" + ', how are' << ' you?',
+      'concatenation mix went wrong')
   end
-  
-  def method_name
-    
+
+  def test_condition
+    assert_equal('In if',condition('if'))
+    assert_equal('In elsif',condition('elsif'))
+    assert_equal('In else',condition('something'))
+  end
+
+#--------------------------------------------------------------------------
+# helpers  
+#--------------------------------------------------------------------------
+  def condition(value)
+    if(value == 'if')
+        return 'In if'
+    elsif (value == 'elsif')
+        return 'In elsif'
+    else
+        return 'In else'
+    end
   end
 end
