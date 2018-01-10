fd14bcc1549d4054a06ed919668359d0590897b3
Christian Breitkreutz <cbreitkreutz@epages.com>
2018-01-10 22:16:06 +0100

    more basic stuff

--- test/BasicStuff.rb
+++ test/BasicStuff.rb
@@ -10,13 +10,45 @@ class BasicStuff < Test::Unit::TestCase
       "Hello #{ruby}" + ', how are' << ' you?',
       'concatenation mix went wrong')
   end
-
+#--------------------------------------------------------------------------
   def test_condition
     assert_equal('In if',condition('if'))
     assert_equal('In elsif',condition('elsif'))
     assert_equal('In else',condition('something'))
   end
-
+#--------------------------------------------------------------------------
+  def test_array_condition
+    array = ['one','two','three']
+    assert_boolean(array.include?('two'))
+    assert_false(array.include?('zwei'))
+  end
+#--------------------------------------------------------------------------
+  def test_loop_I
+    array = ['one','two','three']
+    collector = ''
+    for element in array
+      collector += element
+    end
+    assert_equal('onetwothree',collector)
+  end
+#--------------------------------------------------------------------------
+  def test_loop_II
+    hash = { :autoquoteKey => 1, :'two' => 2 }
+    merge = ''
+    for key, value in hash
+      merge += key.to_s + value.to_s
+    end
+    assert_equal('autoquoteKey1two2',merge)
+  end
+#--------------------------------------------------------------------------
+  def test_loop_III
+    range = 1..5
+    factorial = 1
+    for i in range
+      factorial *= i
+    end
+    assert_equal(120,factorial)
+  end
 #--------------------------------------------------------------------------
 # helpers  
 #--------------------------------------------------------------------------
